02c34342551f4ae456d1cf9e2ef3621eef045616
jnbaker <jnbaker89@gmail.com>
2013-06-22 14:50:34 -0400

    syncing latest project

---
+++ TaskListApp/Task List App/SuperTaskList/SuperTaskList/QCCustomCell.h
@@ -0,0 +1,15 @@
+//
+//  QCCustomCell.h
+//  SuperTaskList
+//
+//  Created by QL Mac Lab on 6/18/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCCustomCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+
+@end
